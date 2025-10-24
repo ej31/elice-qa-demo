62f0bc98b614d8adb13fde3b0e4999c28a00dfd9
jeff <itj1032@gmail.com>
2025-10-24 13:29:52 +0900

    chore: demo

--- src/gugudan.py
+++ src/gugudan.py
@@ -13,7 +13,7 @@ def print_two_times_table() -> None:
     print("구구단 2단")
     for line in generate_two_times_table():
         print(line)
-
+    print("안녕하세요! 수업중이에요! 반가워요! 이게 실행되었다면 CI 가 완료된겁니다!")
 
 if __name__ == "__main__":
     print_two_times_table()
